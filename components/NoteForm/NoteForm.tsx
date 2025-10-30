3802b5a14e2dc634ca833b9651dd2f48793919f8
Alexandra100layrenko <66309809+Alexandra100layrenko@users.noreply.github.com>
2025-10-30 15:48:57 +0000

    types

--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -6,7 +6,7 @@ import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { createNote } from '@/lib/api'
-import type { NoteTag } from '@/app/types/note';
+import type { NoteTag } from '@/types/note';
 import css from './NoteForm.module.css';
 
 interface NoteFormProps {
